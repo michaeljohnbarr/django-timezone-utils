80e1d6919fd574b1bb2ff689682727e28d424142
Michael Barr <micbarr@gmail.com>
2016-10-29 13:12:02 -0400

    Added contributor / Updated Copyright year

--- docs/conf.py
+++ docs/conf.py
@@ -44,7 +44,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Django Timezone Utils'
-copyright = u'2015, Michael Barr'
+copyright = u'2016, Michael Barr'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
